61e44a015846df9060c20a1ea80afd3028baa5eb
Dev Mishra <mdev2424@gmail.com>
2024-04-20 23:01:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/reconnect/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.reconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+   CalendarView calendarView;
+   Calendar calendar;
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        calendarView = findViewById(R.id.calendarView);
+        calendar= Calendar.getInstance();
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+
+            }
+
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
